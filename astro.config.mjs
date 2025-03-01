e4ab72c338f21368bbd07a42ed88f89bfa64c2c6
rey93 <reinaldop.rios93@outlook.com>
2025-02-28 22:18:04 -0500

    responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
-    site: 'https://rey93.github.io',
-    base: 'Portafolio',
+    plugins: [tailwindcss()]
   }
 });
